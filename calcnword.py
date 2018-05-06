0f98a976b89b60431ee6d8ab02860bd599dcf0f2
cc <c@freelancecn.com>
2018-05-06 17:41:16 +0800

    word count

---
+++ calcnword.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import jieba
+import os
+def getText(text):
+    txt = open(text,'r',encoding='utf-8').read()
+    txt = txt.lower()
+    for ch in '!~@#$%^&*()_-{}|:"?><[]\';.,/\\':
+        txt = txt.replace(ch," ")
+    return txt
+
+
+filepath = input('please input the file path:')
+filename = os.path.basename(filepath)
+wordtxt = getText(filename)
+words = jieba.lcut(wordtxt)
+counts = {}
+for word in words:
+    if len(word) == 1:
+        continue
+    else:
+        counts[word] = counts.get(word,0) + 1
+
+items = list(counts.items())
+items.sort(key=lambda x:x[1],reverse=True)
+for i in range(15):
+    word,count = items[i]
+    print('{0:<10}{1:>5}'.format(word,count))
---
+++ calenword.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+def getText(text):
+    txt = open(text,'r').read()
+    txt = txt.lower()
+    for ch in '!~@#$%^&*()_-{}|:"?><[]\';.,/\\':
+        txt = txt.replace(ch," ")
+    return txt
+
+
+filepath = input('please input the file path:')
+filename = os.path.basename(filepath)
+wordtxt = getText(filename)
+words = wordtxt.split()
+counts = {}
+for word in words:
+    counts[word] = counts.get(word,0) + 1
+
+items = list(counts.items())
+items.sort(key=lambda x:x[1],reverse=True)
+for i in range(10):
+    word,count = items[i]
+    print('{0:<10}{1:>5}'.format(word,count))
